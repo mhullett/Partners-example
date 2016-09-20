731a93f25db3281dbad8a87b8f1420f32ac51ada
Matt <Matt@192.168.133.161>
2016-09-19 20:03:17 -0400

    nope

--- partnersExample/src/partnersExample/HelloWorld.java
+++ partnersExample/src/partnersExample/HelloWorld.java
@@ -1,10 +1,13 @@
 package partnersExample;
 
+import org.omg.CORBA.SystemException;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.print("Hello World! ");
 		System.out.println("Hi");
+		System.out.println("This is not a message ");
 
 	}
 
